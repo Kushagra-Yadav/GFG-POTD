3b22dc37835ac964bd052883f97fd8229d9cda7c
Kushagra Yadav <137766817+Kushagra-Yadav@users.noreply.github.com>
2024-03-05 23:31:36 +0530

    05-03-2024.cpp
    Maximum Index

---
+++ 05-03-2024.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+        
+    // A[]: input array
+    // N: size of array
+    // Function to find the maximum index difference.
+    int maxIndexDiff(int a[], int n) 
+    { 
+        vector<int>min_arr(n);
+        vector<int>max_arr(n);
+        int minm=INT_MAX;int maxm=INT_MIN;
+        for(int i=0;i<n;i++)
+        {
+            minm=min(minm,a[i]);
+            min_arr[i]=minm;
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+            maxm=max(maxm,a[i]);
+            max_arr[i]=maxm;
+        }
+        int i=0;int j=0;int ans=-1;
+        while(i<n && j<n)
+        {
+            if(min_arr[i]<=max_arr[j])
+            {
+                ans=max(ans,j-i);
+                j++;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return ans;// Your code here
+    }
+};
+
+//{ Driver Code Starts.
+  
+/* Driver program to test above functions */
+int main() 
+{
+    int T;
+    //testcases
+    cin>>T;
+    while(T--){
+        int num;
+        //size of array
+        cin>>num;
+        int arr[num];
+        
+        //inserting elements
+        for (int i = 0; i<num; i++)
+            cin>>arr[i];
+        Solution ob;
+        
+        //calling maxIndexDiff() function
+        cout<<ob.maxIndexDiff(arr, num)<<endl;    
+        
+    }
+    return 0;
+} 
+// } Driver Code Ends
